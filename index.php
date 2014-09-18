48ba4891c900c8d2bc09ae0673cb43bc3e1ba568
brayan acebo <brayan.acebo@imaginamos.co>
2014-09-18 17:12:08 -0500

    Subida nueva, sin cosas de ima

--- index.php
+++ index.php
@@ -43,13 +43,13 @@
       </figure>
        <a href="#home" class="btn logo fleft ani-cubic" title="Brayan Acebo">Brayan Acebo</a>
        <div class="fright">
-        <a class="btn ani current mayus" href="#intro">Servicios</a>
+        <a class="btn ani current mayus" href="#intro">Sobre mi</a>
         &nbsp;&nbsp;/&nbsp;&nbsp;
-        <a class="btn ani mayus" href="#soluciones">Soluciones</a>
-        &nbsp;&nbsp;/&nbsp;&nbsp;
-        <a class="btn ani mayus" href="#demos">Demos</a>
-        &nbsp;&nbsp;/&nbsp;&nbsp;
-        <a class="btn ani mayus" href="#clientes">Experiencia</a>
+        <a class="btn ani mayus" href="#soluciones">Conocimiento</a>
+        <!--&nbsp;&nbsp;/&nbsp;&nbsp;
+        <a class="btn ani mayus" href="#demos">Demos</a>-->
+        <!--&nbsp;&nbsp;/&nbsp;&nbsp;
+        <!--<a class="btn ani mayus" href="#clientes">Experiencia</a>-->
         &nbsp;&nbsp;/&nbsp;&nbsp;
         <a class="btn ani mayus" href="#contactar">Contactar</a>
       </div>
@@ -64,10 +64,10 @@
   <div id="menur" class="ani-cubic ocult">
     <span class="icon-menu2 burguer ani-cubic"></span>
     <a class="btn ani mayus" href="#home">Inicio</a>
-    <a class="btn ani mayus" href="#intro">Servicios</a>
-    <a class="btn ani mayus" href="#soluciones">Soluciones</a>
-    <a class="btn ani mayus" href="#demos">Demos</a>
-    <a class="btn ani mayus" href="#clientes">Experiencia</a>
+    <a class="btn ani mayus" href="#intro">Sobre mi</a>
+    <a class="btn ani mayus" href="#soluciones">Conocimiento</a>
+    <!--<a class="btn ani mayus" href="#demos">Demos</a>
+    <a class="btn ani mayus" href="#clientes">Experiencia</a>-->
     <a class="btn ani mayus" href="#contactar">Contactar</a>
   </div>
 </nav>
@@ -86,7 +86,7 @@
           <!-- <p class="lenguage"><a href="http://es.brayanacebo.com">ES</a> | <a href="http://en.brayanacebo.com">EN</a></p> -->
             <a id="badge" class="btn" href="#intro" style="margin-left: -440px;">
              <h2 class="" style="margin-top: 0px; opacity: 1;">Brayan Acebo</h2>
-             <h3 id="home-intro" style="margin-top: 10px; opacity: 1;">apps - webs - tutoriales - blogs</h3>
+             <h3 id="home-intro" style="margin-top: 10px; opacity: 1;">Saber cómo la gente va a usar algo es esencial</h3>
            </a>
            <div id="c1" style="opacity: 1;">
             <div class="bottom-btns dawn delay5 floating" style="top: 485px; display: block;">
@@ -108,11 +108,19 @@
  <section>
   <div id="intro" class="ap">
     <div class="wrap">
-      <h1 class="wellcome delay1 dawn"><b>Aplicaciones móvil</b> y <b>Sitios web</b> para profesionales, negocios y startups. Tiendas para <b>comercio electrónico</b>, sitios corporativos, gestores de contenido <b>CMS</b>, galerías de imágenes, portfolios y blogs corporativos. Configurables, <b>responsables</b> y optimizados para tablets y smartphones. Además para colegas <b>tutoriales</b>, foros, <b>tips</b>..</h1>
+
+      <h1 class="wellcome delay1 dawn">
+        Soy ingeniero de sistemas, y llevo mas de 5 años ejerciendo esta hermosa labor. Principalmente 
+        desarrollando <b>software web</b> pero también atreviéndome a liderar equipos de trabajo y manejando 
+        proyectos con clientes pequeños, medianos y gigantes de la misma manera; Pienso que todo debe 
+        hacerse lo más simple posible, pero <b>no más sencillo</b> y mi objetivo principal en todo lo que hago es 
+        la <b>calidad</b>. Me apasiona programar para <b>dispositivos móviles</b>, aprender todos los días y retar 
+        mis conocimientos. 
+      </h1>
+
     </div>
   </div>
 </section>
-
 
 <!-- servicios -->
 <section>
@@ -143,7 +151,7 @@
   <div id="soluciones" class="ap">
     <div class="wrap">
       <div id="leftsol">
-       <h2>Todo lo necesario<span>Sitios autogestionables, adaptables a dispositivos móviles y optimizados para buscadores.</span><span class="miniraya"></span></h2>
+       <h2>Mayor experiencia<span>En desarrollo de sitios autogestionables, adaptables a dispositivos móviles y optimizados para buscadores.</span><span class="miniraya"></span></h2>
        <div id="visor" class="wellcome ani dawn">
         <div class="browser"></div>
         <img class="1 a" src="./images/tipo1.jpg" style="display: block;">
@@ -155,12 +163,15 @@
 
      <div itemscope="" itemtype="http://schema.org/Product" class="tipo wellcome delay1 dawn" data-tipo="1">
      <h3 itemprop="name" class="active"><b>Ecommerce</b>, tiendas online</h3>
-     <p itemprop="description">Soluciones con carro de compra, pasarela de pago, control de stock. Vende fácilmente lo que quieras y aumenta tus ventas.</p>
+     <p itemprop="description">He logrado desarrollar en mi carrera una gran cantidad de soluciones con carro de compra, pasarela de pago, control de stock, etc..</p>
      </div>
 
-     <div itemscope="" itemtype="http://schema.org/Product" class="tipo wellcome delay2 dawn" data-tipo="2"><h3 itemprop="name" class=""><b>Sitios y blogs</b> corporativos</h3><p itemprop="description">La imagen de tu negocio. Mejora la confianza de tus clientes publicando tu contenido. Gestión de comunicación y comentarios.</p></div>
+     <div itemscope="" itemtype="http://schema.org/Product" class="tipo wellcome delay2 dawn" data-tipo="2"><h3 itemprop="name" class=""><b>Sitios y blogs</b> corporativos</h3>
+      <p itemprop="description">Es la imagen del negocio. E visto como mejora la confianza de los clientes publicando contenido. Con gestión de comunicación y comentarios.</p></div>
 
-     <div itemscope="" itemtype="http://schema.org/Product" class="tipo wellcome delay3 dawn" data-tipo="3" style="margin-bottom:20px"><h3 itemprop="name" class=""><b>Galerías</b> y portfolios</h3><p itemprop="description">Sitios para fotografía, ilustración, etc. Con un diseño pensado para que tus fotos se vean espectaculares en cualquier dispositivo.</p></div>
+     <div itemscope="" itemtype="http://schema.org/Product" class="tipo wellcome delay3 dawn" data-tipo="3" style="margin-bottom:20px"><h3 itemprop="name" class=""><b>Galerías</b> y portfolios</h3>
+     <p itemprop="description">Sitios para fotografía, ilustración, etc. Con un diseño pensado para que las fotos se vean espectaculares en cualquier dispositivo y animaciones perfectas que le dan vida.</p></div>
+
      <a class="buto btn buap ani wellcome delay5 dawn" href="#extras">Más información</a>
    </div>
    <div class="clear"></div>
@@ -171,18 +182,18 @@
 <section>
   <div id="extras" class="ap alternate">
     <div class="wrap">
-      <h2>Todo incluído<span>Me encargo de todo, con un resultado profesional y responsable.</span><span class="miniraya"></span></h2>
+      <h2>Mis armas de trabajo<span>El tiempo me a dado para aprender de todo un poco con relación a esta profesión y así poder destacarme con</span><span class="miniraya"></span></h2>
 
       <div itemscope="" itemtype="http://schema.org/Product" class="service wellcome dawn">
       <span class="icon-pen service-icons"></span>
       <h3 itemprop="name"><b>Usabilidad</b> y diseño</h3>
-      <p itemprop="description">Todo a medida según las necesidades o plantilla predefinida (demos).</p>
+      <p itemprop="description">Todo a medida según las necesidades.</p>
       </div>
 
       <div itemscope="" itemtype="http://schema.org/Product" class="service wellcome delay1 dawn">
       <span class="icon-code service-icons"></span>
       <h3 itemprop="name"><b>Desarrollo</b> web</h3>
-      <p itemprop="description">Tecnologías HTML5, CSS3, JQuery, PHP, MySQL. Fuertes <b>Node.js, Angular, Laravel, Stylus.</b></p>
+      <p itemprop="description">Tecnologías HTML5, CSS3, JQuery, PHP, MySQL. Fuertes <b>MongoDB, Node.js, Angular, Laravel, Stylus.</b></p>
       </div>
 
       <div itemscope="" itemtype="http://schema.org/Product" class="service wellcome delay2 nomr dawn">
@@ -194,12 +205,12 @@
       <div itemscope="" itemtype="http://schema.org/Product" class="service wellcome delay3 dawn">
       <span class="icon-equalizer service-icons"></span>
       <h3 itemprop="name"><b>Gestión</b> contenidos CMS</h3>
-      <p itemprop="description">Modifique el contenido de su sitio web mediante un backweb de gestión.</p>
+      <p itemprop="description">Desarrollo de backend para la administracción de contenidos.</p>
       </div>
 
       <div itemscope="" itemtype="http://schema.org/Product" class="service wellcome delay4 dawn">
       <span class="icon-cloud service-icons"></span>
-      <h3 itemprop="name"><b>Dominio</b> y hosting</h3><p itemprop="description">Nos encargamos de gestionar su dominio y alojamiento web.</p></div>
+      <h3 itemprop="name"><b>Dominio</b> y hosting</h3><p itemprop="description">Gestión de dominios y alojamiento web.</p></div>
 
       <div itemscope="" itemtype="http://schema.org/Product" class="service wellcome delay5 nomr dawn">
       <span class="icon-mobile2 service-icons"></span>
@@ -213,7 +224,7 @@
 </section>
 
 <!-- demos -->
-<section>
+<!--<section>
   <div id="demos" class="ap">
     <div class="wrap">
 
@@ -228,7 +239,7 @@
         </figure>
       </div>
 
-     <!--  <div class="fleft wellcome dawn">
+       <div class="fleft wellcome dawn">
         <div class="browser"></div>
         <div class="demo d1" style="background-image: url(./i/s1.jpg); display: block;">
           <a href="#demoviewer" data-rel="modal" class="ani" data-demo="1"><span class="ani-cubic">sitio corporativo, blog, portfolio</span></a>
@@ -240,57 +251,57 @@
         <div class="demo d2 nomr" style="background-image: url(./i/s2.jpg); display: block;">
           <a href="#demoviewer" data-rel="modal" class="ani" data-demo="2"><span class="ani-cubic">portfolio, blog</span></a>
         </div>
-      </div> -->
+      </div> 
 
       <div class="clear"></div>
     </div>
   </div>
-</section>
+</section>-->
 
 
 <!-- clientes -->
-<section>
+<!--<section>
   <div id="clientes" class="ap">
     <div class="wrap">
       <h2>Experiencia<span>Ultimos proyectos trabajados y destacados</span><span class="miniraya"></span></h2>
     </div>
 
-    <!-- cliente 1 -->
+    
     <div class="job wellcome dawn">
       <img width="465" height="350" src="./images/work1.png" alt="Reconciliación Colombia, Freelance Brayan Acebo" style="display: inline;">
       <h4>Reconciliación Colombia</h4>
       <p>Sitio web de la <b>Revista Semana</b>; desarrollado dentro de imaginamos.com</p>
     </div>
 
-    <!-- cliente 2 -->
+    
     <div class="job wellcome delay1 dawn">
       <img height="350" src="./images/work2.png" alt="idearama.co, Freelance Brayan Acebo" style="display: inline;">
       <h4>idearama.co</h4>
       <p>Diseño de logos, páginas web... y mucho más! Hace poco mencionado en <b>ENTER.CO</b></p>
     </div>
 
-    <!-- cliente 3 -->
+    
     <div class="job wellcome dawn">
       <img width="465" height="350" src="./images/work3.png" alt="Reconciliación Colombia, Freelance Brayan Acebo" style="display: inline;">
       <h4>Pulpo Paul</h4>
       <p>"Polla Mundialista" que estara este año; se destaca <b>programación en tiempo real.</b></p>
     </div>
 
-    <!-- cliente 4 -->
+    
     <div class="job wellcome dawn">
       <img width="465" height="350" src="./images/work4.png" alt="Imaginamos, Freelance Brayan Acebo" style="display: inline;">
       <h4>Imaginamos</h4>
       <p>Desarrollo <b>CMS</b> estandar en PYRO (codeigniter) para Imaginamos.</p>
     </div>
 
-    <!-- cliente 5 -->
+    
     <div class="job wellcome dawn">
       <img height="350" src="./images/work5.png" alt="Ping, Freelance Brayan Acebo" style="display: inline;width: 170px">
       <h4>Ping</h4>
       <p><b>Aplicación Móvil</b> para la gestión de proyectos via comentarios.</p>
     </div>
 
-    <!-- cliente 6 -->
+    
     <div class="job wellcome dawn">
       <img width="465" height="350" src="./images/work6.png" alt="Eventos Uniandinos, Freelance Brayan Acebo" style="display: inline;">
       <h4>Eventos Uniandinos</h4>
@@ -299,24 +310,24 @@
 
    <div class="clear"></div>
  </div>
-</section>
+</section>-->
 
 <!-- contactar -->
 <section>
   <div id="contactar" class="ap">
     <div class="wrap">
-      <h2 style="text-align:left">Empezar un proyecto<span>Si tienes un proyecto en mente, deseas renovar tu sitio o necesitas un compañero no dudes en contactarme.</span><span class="miniraya"></span></h2>
+      <h2 style="text-align:left">Aquí<span>Me podes encontrar fácilmente si lo necesitas, siempre estaré agradecido que cuentes conmigo.</span><span class="miniraya"></span></h2>
       <div class="form fleft">
         <form action="sendmail.php" method="post" id="ancla">
           <input name="firstname" type="text" class="nom ani" placeholder="Nombre">
           <input name="email" type="text" class="mail ani" placeholder="Email" style="margin-right:0">
           <input name="phone" type="text" class="telf ani" placeholder="Teléfono" maxlength="9">
-          <input name="company" type="text" class="comp ani" placeholder="Compañia / empresa" style="margin-right:0">
+          <!-- <input name="company" type="text" class="comp ani" placeholder="Compañia / empresa" style="margin-right:0"> -->
           <textarea name="text" class="msg ani" rows="20" cols="40" placeholder="Explica brevemente qué necesitas"></textarea>
           <div class="message">
-          <div id="alert">
+          <!-- <div id="alert">
             <h5><b>Disponibilidad:</b> <i>proyectos freelance</i></h5>
-          </div>
+          </div> -->
           </div>
           <input type="submit" value="Enviar" class="buto fright ani">
           <div class="clear"></div>
@@ -333,7 +344,17 @@
       <hr>
 
       <div class="me fright" itemscope="" itemtype="http://schema.org/LocalBusiness">
-        <p style="line-height: 27px;">Al recibir tu mensaje, <b>analizare y estudiare tu idea</b>. A la mayor brevedad posible recibirás una respuesta. Si lo que necesitas es un presupuesto, me pondre en contacto contigo, para analizar mejor todos los detalles.</p>
+        <p style="line-height: 27px;">
+          Creo que <b>compartir el conocimiento es una virtud</b> que no todos tienen y vale mucho, como lo 
+          viste antes me gusta estudiar, conocer y aprender constantemente. No dudes en contactarme si 
+          en algo como esto puedo ser útil. 
+        </p>
+
+        <!--
+  Al recibir tu mensaje, <b>analizare y estudiare tu idea</b>. A la mayor brevedad posible recibirás una respuesta. Si lo que necesitas es un presupuesto, me pondre en contacto contigo, para analizar mejor todos los detalles.
+        -->
+
+
         <br>
 
         <p itemprop="address" itemscope="" itemtype="http://schema.org/PostalAddress">
@@ -351,9 +372,9 @@
         <a class="me3 ani" href="tel:3162719960">316 271 9960</a>
         </p>
         <div align="center">
-          <a href="https://facebook.com/brayanacebo" target="_blank"><span class="icon-facebook2 social-icons"></span></a>
+          <!-- <a href="https://facebook.com/brayanacebo" target="_blank"><span class="icon-facebook2 social-icons"></span></a> -->
           <a href="https://twitter.com/BrayanAcebo" target="_blank"><span class="icon-twitter social-icons"></span></a>
-          <a href="https://bitbucket.org/brayanacebo" target="_blank"><span class="icon-github5 social-icons"></span></a>
+          <a href="https://github.com/brayanacebo" target="_blank"><span class="icon-github5 social-icons"></span></a>
           <a href="http://www.linkedin.com/profile/view?id=178871412&trk=nav_responsive_tab_profile" target="_blank"><span class="icon-linkedin social-icons"></span></a>
         </div>
         </p>
